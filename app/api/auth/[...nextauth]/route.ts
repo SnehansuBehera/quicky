6c8f5d60cc1a1fed7a9f9f98d068c6679eb5af03
SnehansuBehera <snehansbehera080903@gmail.com>
2024-07-19 10:14:02 +0530

    project-almost-done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
+import type { NextAuthOptions } from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
@@ -8,7 +9,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from "@/app/libs/prismadb";
 
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
