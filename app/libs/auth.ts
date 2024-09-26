7941899d77a1d64393266d2ca0aa08a38a9474ab
SnehansuBehera <snehansbehera080903@gmail.com>
2024-09-26 10:37:14 +0530

    some fixes

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -53,7 +53,7 @@ export const authOptions: NextAuthOptions = {
         })
 
     ],
-    debug: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === 'production',
     session: {
         strategy: "jwt"
     },
