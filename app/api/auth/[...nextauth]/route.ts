a8180733d3b56fc05b85287f82e9daaf59b370ec
SnehansuBehera <snehansbehera080903@gmail.com>
2024-10-03 19:54:01 +0530

    few-changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/app/libs/auth";
 
-const handler = async (req: any, res: any) => {
-    try {
-        await NextAuth(req, res, authOptions);
-    } catch (error) {
-        console.error('NextAuth Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
-};
+
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
